0e45b224afcb499673a151d3b0c49efe85dbfda2
Rahulsarma2003 <sharmarahul32578@gmail.com>
2025-11-29 17:32:31 +0530

    bug fixed

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -3,10 +3,11 @@ import { useAppContext } from '../context/AppContext';
 import { useNavigate } from 'react-router-dom';
 import GoogleIcon from '../assets/GoogleIcon';
 
-const API_BASE_URL = import.meta.env.VITE_API_URL;
-//const API_BASE = "http://localhost:8000";
+//const API_BASE_URL = import.meta.env.VITE_API_URL;
 
-console.log("API URL:", API_BASE_URL);//testing 
+const API_BASE_URL = "http://localhost:8000";
+
+console.log("API URL:", API_BASE_URL); //testing 
 
 const Login = () => {
     // local states for form handling
