462f1714704ea12dc10c5feeace3a64e9001d92e
Rahulsarma2003 <sharmarahul32578@gmail.com>
2025-12-06 00:15:12 +0530

    login/signup page updated

--- src/api/pdfApi.js
+++ src/api/pdfApi.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 
-//const API_BASE_URL = import.meta.env.VITE_API_URL;
-const API_BASE_URL = "http://localhost:8000";
+const API_BASE_URL = import.meta.env.VITE_API_URL;
+//const API_BASE_URL = "http://localhost:8000";
 
 // Create axios instance with default config
 const api = axios.create({
